c93d78253c43df48a5a18272131a68bb7e24c241
Emelie Ardby <emelie.ardby@schibsted.se>
2017-09-26 15:26:06 +0200

    uppdaterad med räknare

--- store.js
+++ store.js
@@ -2,3 +2,15 @@ const greet = () => {
 	console.log ('välkommen')
 	}
 	greet ()
+
+const counter = () => {
+	console.log ('115')
+}
+	counter ()
+
+const banana = '50'
+const spoon = '3'
+const counter1 = () => {
+	console.log (banana*2 + spoon*5)
+}
+	counter1 ()
