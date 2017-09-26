2ba9f65df74be3b59e84270761c6bb360aea5e46
Emelie Ardby <emelie.ardby@schibsted.se>
2017-09-26 15:54:28 +0200

    mer ändringar

--- store.js
+++ store.js
@@ -14,3 +14,26 @@ const counter1 = () => {
 	console.log (banana*2 + spoon*5)
 }
 	counter1 ()
+
+const Banana2 = {
+	name: 'Bananasplit',
+	price: 50
+
+}
+const Spoon2 = {
+	name: 'spoon',
+	price: 3
+
+}
+
+const DescribeBanana2 = () => {
+	console.log (Banana2.name + Banana2.price)
+}
+DescribeBanana2 ()
+
+const DescribeSpoon2 = () => {
+	console.log (Spoon2.name + Spoon2.price)
+}
+DescribeSpoon2 ()
+
+
